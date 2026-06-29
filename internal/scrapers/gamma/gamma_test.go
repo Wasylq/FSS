93354addf291142d52666a9d24894ec9226e1964
Wasylq <wasylq@protonmail.com>
2026-06-29 08:30:00 +0200

    Fix gamma site count test

--- internal/scrapers/gamma/gamma_test.go
+++ internal/scrapers/gamma/gamma_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestSiteCount(t *testing.T) {
-	if len(sites) != 155 {
-		t.Errorf("expected 155 sites, got %d", len(sites))
+	if len(sites) != 167 {
+		t.Errorf("expected 167 sites, got %d", len(sites))
 	}
 }
 
